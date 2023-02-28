81253255b8f2337acb26d5fe19987783b8c08773
Min Yeong Park <yingo24655@naver.com>
2023-02-28 23:24:35 +0900

    [Bronze V] Title: 꼬마 정민, Time: 124 ms, Memory: 14076 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long a = Long.parseLong(st.nextToken());
+        long b = Long.parseLong(st.nextToken());
+        long c = Long.parseLong(st.nextToken());
+        System.out.println(a+b+c);
+    }
+}
