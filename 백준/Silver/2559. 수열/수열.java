e36f3d04ca116e82602cc7c9107d006280415a95
Min Yeong Park <yingo24655@naver.com>
2023-03-04 01:30:29 +0900

    [Silver III] Title: 수열, Time: 264 ms, Memory: 24308 KB -BaekjoonHub

---
+++ 백준/Silver/2559. 수열/수열.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int[] sum_arr = new int[n - k + 1];
+
+        for (int i = 0; i < k; i++) {
+            sum_arr[0] += arr[i];
+        }
+
+        for (int i = 1; i < sum_arr.length; i++) {
+            sum_arr[i] = sum_arr[i-1] - arr[i-1] + arr[i-1+k];
+        }
+
+        int max = -999999999;
+        for (int i = 0; i < sum_arr.length; i++) {
+            if (sum_arr[i] >= max)
+                max = sum_arr[i];
+        }
+
+        System.out.println(max);
+    }
+}
