9731072da0c800b88ffae009a82b8217a80e882b
Min Yeong Park <yingo24655@naver.com>
2023-03-05 21:04:34 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 856 ms, Memory: 121876 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -8,13 +8,20 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int[][] arr = new int[n+1][n+1];
+        int[][] dp = new int[n+1][n+1];
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 1; j <= n; j++) {
-                arr[i][j] = arr[i][j-1] + Integer.parseInt(st.nextToken()); // 행 별로 누적합 저장
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + arr[i][j];
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
@@ -23,11 +30,7 @@ public class Main {
             int x2 = Integer.parseInt(st.nextToken());
             int y2 = Integer.parseInt(st.nextToken());
 
-            int sum = 0;
-            for (int x = x1; x <= x2; x++) {
-                sum += arr[x][y2] - arr[x][y1-1];
-            }
-            sb.append(sum).append("\n");
+            sb.append(dp[x2][y2] - dp[x2][y1-1] - dp[x1-1][y2] + dp[x1-1][y1-1]).append("\n");
         }
         System.out.println(sb);
     }
