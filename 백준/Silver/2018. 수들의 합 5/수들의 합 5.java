9a7be480f662ba8acd80624899b7958d407258a0
Min Yeong Park <yingo24655@naver.com>
2023-03-05 19:39:36 +0900

    [Silver V] Title: 수들의 합 5, Time: 164 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -4,24 +4,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n+1];
-        for (int i = 1; i <= n; i++) {
-            arr[i] = arr[i-1] + i;
-        }
 
         int start_idx = 1;
         int end_idx = 1;
-        int count = 0;
+        int count = 1;
+        int sum = 1;
 
-        while (start_idx <= n && end_idx <= n) {
-            int sum = arr[end_idx] - arr[start_idx-1];
+        while (end_idx != n) {
             if (sum < n) {
                 end_idx++;
+                sum = sum + end_idx;
             } else if (sum > n) {
-                start_idx++;
-            } else {
+                sum = sum - start_idx; // 기존에 있었던 것을 빼주고
+                start_idx++;           // 인덱스를 증가
+            } else { // 같은 경우
                 count++;
                 end_idx++;
+                sum = sum + end_idx;
             }
         }
 
