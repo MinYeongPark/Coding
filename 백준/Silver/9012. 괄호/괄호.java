34b9c9811652aec1b0145a840a6230a00e7aeb4f
Min Yeong Park <yingo24655@naver.com>
2023-03-08 23:22:14 +0900

    [Silver IV] Title: 괄호, Time: 136 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,30 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        Stack<Character> stack;
+        int n = Integer.parseInt(br.readLine());
+        Stack<Character> s;
+        String line;
         Loop1:
-        for (int i = 0; i < t; i++) {
-            String str = br.readLine();
-            stack = new Stack();
+        for (int i = 0; i < n; i++) {
+            s = new Stack<>();
+            line = br.readLine();
             Loop2:
-            for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                if (c == '(')
-                    stack.push(c);
-                else if (stack.isEmpty()) {
-                    System.out.println("NO");
-                    continue Loop1;
-                } else {
-                    stack.pop();
+            for (int j = 0; j < line.length(); j++) {
+                if (line.charAt(j) == '(') {
+                    s.push('(');
+                } else if (line.charAt(j) == ')') {
+                    if (s.isEmpty()) {
+                        System.out.println("NO");
+                        continue Loop1;
+                    }
+                    else if (s.peek() == '(') {
+                        s.pop();
+                    } else {
+                        s.push(')');
+                    }
                 }
             }
-            if (stack.isEmpty()) {
+
+            if (s.isEmpty()) {
                 System.out.println("YES");
             } else {
                 System.out.println("NO");
