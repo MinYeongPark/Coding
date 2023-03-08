a16a17ce9386828bcab7dea47334eeafc29c9cd9
Min Yeong Park <yingo24655@naver.com>
2023-03-09 06:40:21 +0900

    [Silver IV] Title: 카드2, Time: 188 ms, Memory: 45624 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() >= 2) {
+            q.poll();
+            if (!q.isEmpty()) {
+                q.offer(q.poll());
+            }
+        }
+
+        System.out.println(q.poll());
+    }
+}
