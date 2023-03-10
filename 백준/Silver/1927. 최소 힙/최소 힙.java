05f70204250c10f483767345571752c4ae84c63e
Min Yeong Park <yingo24655@naver.com>
2023-03-11 00:51:26 +0900

    [Silver II] Title: 최소 힙, Time: 1116 ms, Memory: 33656 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < n; i++) {
+            int x = Integer.parseInt(br.readLine());
+            if (x == 0) {
+                if (!pq.isEmpty()) {
+                    System.out.println(pq.poll());
+                } else {
+                    System.out.println(0);
+                }
+            } else {
+                pq.add(x);
+            }
+        }
+    }
+}
