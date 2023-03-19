b4c467816d783598183bd1536f428e7fcc7b78e3
Min Yeong Park <yingo24655@naver.com>
2023-03-20 00:07:08 +0900

    [Silver IV] Title: 동전 0, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] coins = new int[N];
+        for (int i = 0; i < N; i++) {
+            coins[i] = Integer.parseInt(br.readLine());
+        }
+
+        int count = 0;
+        for (int i = N-1; i >= 0; i--) {
+            if (K >= coins[i]) {
+                count += K / coins[i];
+                K = K % coins[i];
+            }
+        }
+
+        System.out.println(count);
+    }
+}
