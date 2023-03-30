9bb5db2a53733009f519211b42667f079cd5182b
Min Yeong Park <yingo24655@naver.com>
2023-03-31 01:50:42 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        long[] dp = new long[1001];
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
+        }
+        System.out.println(dp[n]);
+    }
+}
