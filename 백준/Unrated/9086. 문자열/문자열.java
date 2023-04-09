40ed11446920249a6d1cef53671743bdbbd739ba
Min Yeong Park <yingo24655@naver.com>
2023-04-09 22:02:58 +0900

    [Unrated] Title: 문자열, Time: 144 ms, Memory: 14552 KB -BaekjoonHub

---
+++ 백준/Unrated/9086. 문자열/문자열.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            sb.append(str.charAt(0)).append(str.charAt(str.length() - 1)  + "\n");
+        }
+        System.out.println(sb);
+    }
+}
