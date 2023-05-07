1606a55f77afea4d22201b4ea8f80ff28186d0b0
Min Yeong Park <yingo24655@naver.com>
2023-05-07 23:02:04 +0900

    [Bronze II] Title: 공 바꾸기, Time: 148 ms, Memory: 15856 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        for (int cnt = 1; cnt <= n; cnt++) {
+            arr[cnt] = cnt;
+        }
+        for (int cnt = 0; cnt < m; cnt++) {
+            st = new StringTokenizer(br.readLine());
+            int i = Integer.parseInt(st.nextToken());
+            int j = Integer.parseInt(st.nextToken());
+
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+        for (int cnt = 1; cnt <= n; cnt++) {
+            System.out.print(arr[cnt] + " ");
+        }
+    }
+}
