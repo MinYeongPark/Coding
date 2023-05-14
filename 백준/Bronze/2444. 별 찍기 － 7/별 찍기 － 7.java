4b01bc4f8a9556521f676ef53e3e09951a09ed13
Min Yeong Park <yingo24655@naver.com>
2023-05-14 19:23:14 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 220 ms, Memory: 17836 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        for (int i = 0; i < 2 * n - 1; i++) {
+            sb.append("*");
+        }
+        sb.append("\n");
+
+        for (int i = n - 1; i >= 1; i--) {
+            for (int j = 0; j < n - i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 0; j < 2 * i - 1; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
