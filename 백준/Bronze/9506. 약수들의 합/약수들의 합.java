d49f02ebcee08c3c888685ddfd5b564be0d90997
Min Yeong Park <yingo24655@naver.com>
2023-05-28 19:54:46 +0900

    [Bronze I] Title: 약수들의 합, Time: 144 ms, Memory: 15620 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int input;
+        StringBuilder sb = new StringBuilder();
+        while ((input = Integer.parseInt(br.readLine())) != -1) {
+            int sum = 0;
+            PriorityQueue<Integer> q = new PriorityQueue();
+            for (int i = 1; i < input; i++) {
+                if (input % i == 0) {
+                    sum += i;
+                    q.offer(i);
+                }
+            }
+
+            if (sum == input) {
+                sb.append(input + " = ");
+                int size = q.size();
+                for (int i = 0; i < size - 1; i++) {
+                    sb.append(q.poll() + " + ");
+                }
+                sb.append(q.poll() + "\n");
+            } else {
+                sb.append(input + " is NOT perfect.\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
