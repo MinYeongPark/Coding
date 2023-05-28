d2d2bc2cdfe74cd0d8a36d39087881d09119787f
Min Yeong Park <yingo24655@naver.com>
2023-05-28 19:57:15 +0900

    [Bronze V] Title: 직사각형, Time: 124 ms, Memory: 14160 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.Locale;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        System.out.println(a * b);
+    }
+}
