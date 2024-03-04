2c564f550e6e206c1e0f7f8fb5215cc653419823
MinYeongPark <yingo24655@naver.com>
2024-03-04 19:28:50 +0900

    [Silver V] Title: 셀프 넘버, Time: 120 ms, Memory: 13948 KB -BaekjoonHub

--- 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
+++ 백준/Silver/4673. 셀프 넘버/셀프 넘버.java
@@ -4,20 +4,21 @@ public class Main {
         for (int i = 1; i <= 10000; i++) {
             int next_num = i;
             int temp_num = next_num;
-            while (temp_num >= 10) {
+            while (temp_num != 0) {
                 next_num += temp_num % 10;
                 temp_num /= 10;
             }
-            next_num += temp_num % 10;
             if (next_num <= 10000) {
                 numbers[next_num] = true;
             }
         }
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= 10000; i++) {
             if (!numbers[i]) {
-                System.out.println(i);
+                sb.append(i).append("\n");
             }
         }
+        System.out.println(sb);
     }
 }
