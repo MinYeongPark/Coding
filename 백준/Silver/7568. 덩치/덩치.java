31a109176b59d56219ac9e799dab1d930cdaee30
MinYeongPark <yingo24655@naver.com>
2024-03-07 19:44:34 +0900

    [Silver V] Title: 덩치, Time: 124 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        Person[] list = new Person[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            list[i] = new Person(x, y);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            int rank = 1;
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                if (list[i].x < list[j].x && list[i].y < list[j].y) {
+                    rank++;
+                }
+            }
+            sb.append(rank).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
+
+class Person {
+    int x;
+    int y;
+
+    public Person(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
