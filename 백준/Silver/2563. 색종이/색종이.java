0569899d395f22b10f20ff369261fb06bd2c834f
MinYeongPark <yingo24655@naver.com>
2024-03-09 23:16:59 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] map = new boolean[100][100];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            for (int nx = x; nx < x + 10; nx++) {
+                for (int ny = y; ny < y + 10; ny++) {
+                    map[ny][nx] = true;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+                if (map[i][j])
+                    cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+}
