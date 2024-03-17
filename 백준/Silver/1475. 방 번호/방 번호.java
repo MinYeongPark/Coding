06cb1e209804b12f802ff04b9435967037f7deae
MinYeongPark <yingo24655@naver.com>
2024-03-18 01:44:04 +0900

    [Silver V] Title: 방 번호, Time: 124 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Silver/1475. 방 번호/방 번호.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String room = br.readLine();
+        int[] arr = new int[9]; // 0 ~ 8까지 이용 예정
+        for (int i = 0; i < room.length(); i++) {
+            int num = room.charAt(i) - '0';
+            if (num == 9) {
+                arr[6]++;
+                continue;
+            }
+            arr[num]++;
+        }
+
+        if (arr[6] % 2 == 0) {
+            arr[6] = arr[6] / 2;
+        } else {
+            arr[6] = arr[6] / 2 + 1;
+        }
+
+        int max_count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            max_count = Math.max(arr[i], max_count);
+        }
+        System.out.println(max_count);
+    }
+}
