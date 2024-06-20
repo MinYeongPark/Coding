ef2aa9737dbc825e4bb31c2edc10bbbc09f06454
MinYeongPark <yingo24655@naver.com>
2024-06-21 00:41:34 +0900

    [Silver I] Title: Z, Time: 124 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.java
+++ 백준/Silver/1074. Z/Z.java
@@ -1,38 +1,39 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n, r, c;
-    static int answer;
+    static int n, R, C;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        answer = 0;
-        int size = (int) Math.pow(2, n);
 
-        recur(r, c, size);
-        System.out.println(answer);
+        n = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        
+        System.out.println(recur(n, R, C));
     }
 
-    static void recur(int r, int c, int size) {
-        if (size == 1) {
-            return;
+    static int recur(int n, int r, int c) {
+        if (n == 0) {
+            return 0;
         }
 
-        if (r < size/2 && c < size/2) { // 1사분면에 있는 경우
-            recur(r, c, size/2);
-        } else if (r < size/2 && c >= size/2) { // 2사분면에 있는 경우
-            answer += size * size / 4;
-            recur(r, c - size/2, size/2); // 2사분면에서의 r,c 상대위치를 넘겨주어야 함 (r, c-size/2)
-        } else if (r >= size/2 && c < size/2) { // 3사분면에 있는 경우
-            answer += size * size / 4 * 2;
-            recur(r - size/2, c, size/2); // 3사분면에서의 r,c 상대위치를 넘겨주어야 함 (r-size/2, c)
-        } else { // 4사분면에 있는 경우
-            answer += size * size / 4 * 3;
-            recur(r - size/2, c - size/2, size/2); // 4사분면에서의 r,c 상대위치를 넘겨주어야 함 (r-size/2, c-size/2)
+        int half = (int) Math.pow(2,n-1);
+
+        if (r < half && c < half) {
+            return recur(n-1, r, c);
+        } else if (r < half && c >= half) {
+            return half * half
+                + recur(n-1, r, c-half);
+        } else if (r >= half && c < half) {
+            return 2 * half * half
+                    + recur(n-1, r-half, c);
+        } else {
+            return 3 * half * half
+                    + recur(n-1, r-half, c-half);
         }
     }
 }
