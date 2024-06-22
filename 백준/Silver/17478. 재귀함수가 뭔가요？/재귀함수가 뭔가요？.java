f2040db1ce37b8b3ae1cf24f9c6edab8e955e9e4
MinYeongPark <yingo24655@naver.com>
2024-06-22 23:43:57 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 132 ms, Memory: 14616 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static String underBar = "";
     static StringBuilder sb = new StringBuilder();
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,36 +16,32 @@ public class Main {
     }
 
     static void recur(int now, int cnt) {
-        StringBuilder underBarSB = new StringBuilder();
-        for (int i = 0; i < 4 * now; i++) {
-            underBarSB.append("_");
-        }
+        String tmp = underBar;
 
-        String underBar = underBarSB.toString();
-
-        sb.append(underBar);
+        sb.append(tmp);
         sb.append("\"재귀함수가 뭔가요?\"").append("\n");
 
         if (now == cnt) {
-            sb.append(underBar);
+            sb.append(tmp);
             sb.append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
-            sb.append(underBar);
+            sb.append(tmp);
             sb.append("라고 답변하였지.\n");
             return;
         }
-        
-        sb.append(underBar);
+
+        sb.append(tmp);
         sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
 
-        sb.append(underBar);
+        sb.append(tmp);
         sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
 
-        sb.append(underBar);
+        sb.append(tmp);
         sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
-        
+
+        underBar += "____";
         recur(now + 1, cnt);
 
-        sb.append(underBar);
+        sb.append(tmp);
         sb.append("라고 답변하였지.\n");
     }
 }
