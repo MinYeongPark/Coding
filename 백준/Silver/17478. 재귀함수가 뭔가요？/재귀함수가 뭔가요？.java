e8c724c571028594db13f8aa90cc8a17bf926653
MinYeongPark <yingo24655@naver.com>
2024-06-22 23:37:42 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 136 ms, Memory: 14736 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -1,44 +1,48 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-    static void recur_print(int num, int count) throws IOException {
-        int num_underbar = 0;
-        if (count != 0) {
-            num_underbar = count * 4;
-        }
-        for (int i = 0; i < num_underbar; i++) bw.write("_");
-        bw.write("\"재귀함수가 뭔가요?\"\n");
-
-        if (num == 0) {
-            for (int i = 0; i < num_underbar; i++) bw.write("_");
-            bw.write("\"재귀함수는 자기 자신을 호출하는 함수라네\"\n");
-            for (int i = 0; i < num_underbar; i++) bw.write("_");
-            bw.write("라고 답변하였지.\n");
-            return;
-        }
-        else {
-            for (int i = 0; i < num_underbar; i++) bw.write("_");
-            bw.write("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
-            for (int i = 0; i < num_underbar; i++) bw.write("_");
-            bw.write("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
-            for (int i = 0; i < num_underbar; i++) bw.write("_");
-            bw.write("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
-        }
-
-        recur_print(num - 1, count + 1);
-
-        for (int i = 0; i < num_underbar; i++) bw.write("_");
-        bw.write("라고 답변하였지.\n");
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        recur(0, n);
+        System.out.println(sb);
     }
 
-    public static void main(String[] args) throws IOException{
-        int num = Integer.parseInt(br.readLine());
+    static void recur(int now, int cnt) {
+        if (now == 0) {
+            sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
+        }
 
-        bw.write("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.\n");
-        recur_print(num, 0);
-        bw.flush();
+        StringBuilder underBarSB = new StringBuilder();
+        for (int i = 0; i < 4 * now; i++) {
+            underBarSB.append("_");
+        }
+
+        String underBar = underBarSB.toString();
+
+        sb.append(underBar);
+        sb.append("\"재귀함수가 뭔가요?\"").append("\n");
+
+        if (now == cnt) {
+            sb.append(underBar);
+            sb.append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+        } else {
+            sb.append(underBar);
+            sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.\n");
+
+            sb.append(underBar);
+            sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.\n");
+
+            sb.append(underBar);
+            sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"\n");
+            recur(now + 1, cnt);
+        }
+
+        sb.append(underBar);
+        sb.append("라고 답변하였지.\n");
     }
 }
